747cd323414b61a92fdfeb906afe2ded7f1d0933
harshitrawat199817 <49994498+harshitrawat199817@users.noreply.github.com>
2025-02-07 13:16:58 +0530

    Time: 46 ms (31.00%) | Memory: 97.3 MB (58.51%) - LeetSync

--- 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.java
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.java
@@ -1,25 +1,25 @@
 class Solution {
     public int[] queryResults(int limit, int[][] queries) {
         Map<Integer, Integer> numberToColour = new HashMap<>();
-        Map<Integer, Set<Integer>> colourToNumber = new HashMap<>();
+        Map<Integer, Integer> colourToNumber = new HashMap<>();
         int[] ans = new int[queries.length];
         for(int i = 0; i<queries.length; i++){
             int ball = queries[i][0];
             int colour = queries[i][1];
             if(numberToColour.containsKey(ball)){
                 int prevColour = numberToColour.get(ball);
-                colourToNumber.get(prevColour).remove(ball);
-                if(colourToNumber.get(prevColour).isEmpty()){
+                colourToNumber.put(prevColour, colourToNumber.get(prevColour)-1);
+                if(colourToNumber.get(prevColour)==0){
                     colourToNumber.remove(prevColour);
                 }
             }
             numberToColour.put(ball, colour);
-            if(!colourToNumber.containsKey(colour)){
-                colourToNumber.put(colour, new HashSet<>());
-            }
-            colourToNumber.get(colour).add(ball);
+            // if(!colourToNumber.containsKey(colour)){
+            //     colourToNumber.put(colour, new HashSet<>());
+            // }
+            int freq = colourToNumber.getOrDefault(colour, 0);
+            colourToNumber.put(colour, freq+1);
             ans[i] = colourToNumber.size();
-
         }
         return ans;
     }
